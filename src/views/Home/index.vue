3dab900dadeab6826c25750559de2d3f724325a7
Mardonio Costa <mardonio@live.com>
2022-07-07 15:07:48 -0400

    Criando a tela Inicial

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,42 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>
-    <h1 class="font-black text-brand-main">Aqui é home</h1>
+  <custom-header
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <Contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
   </div>
 </template>
 <script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader'
+import Contact from './Contact'
+
 export default {
+  components: { CustomHeader, Contact },
+
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {}
+
+    function handleAccountCreate () {}
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 
 }
 </script>
