0c7cbfc5bc3577bab934bdb9d3a4be4831b1a402
Mardonio Costa <mardonio@live.com>
2022-07-07 13:46:39 -0400

    Configurando o projeto

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import '@/assets/css/tailwind.css'
+import 'animate.css'
+import '@/assets/css/fonts.css'
+
+const app = createApp(App)
+app.use(store)
+app.use(router).mount('#app')
---
+++ src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1 class="font-black text-brand-main">Aqui é home</h1>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
