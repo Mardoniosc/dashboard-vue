9d1e92cb6a64ca0d33c192e2c5cab726171cbdba
Mardonio Costa <mardonio@live.com>
2022-07-12 13:57:06 -0400

    Fazendo testes e2e na home com cypress

--- src/components/ModalCreateAccount/index.vue
+++ src/components/ModalCreateAccount/index.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template >
-  <div class="flex justify-between">
+  <div class="flex justify-between" id="modal-create-account">
     <h1 class="text-4xl font-black text-gray-800">Crie uma conta</h1>
 
     <button
--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template >
-  <div class="flex justify-between">
+  <div class="flex justify-between" id="modal-login">
     <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
 
     <button
@@ -14,6 +14,7 @@
       <label class="block">
         <span class="text-lg font-medium text-gray-800">E-mail</span>
         <input
+          id="email-field"
           v-model="state.email.value"
           type="email"
           :class="{
@@ -23,6 +24,7 @@
           placeholder="seuemail@gmail.com"
         >
         <span
+          id="email-error"
           v-if="!!state.email.errorMessage"
           class="block font-medium text-brand-danger"
         >
@@ -33,6 +35,7 @@
       <label class="block mt-9">
         <span class="text-lg font-medium text-gray-800">Senha</span>
         <input
+          id="password-field"
           v-model="state.password.value"
           type="password"
           :class="{
@@ -50,6 +53,7 @@
       </label>
 
       <button
+        id="submit-button"
         :disabled="state.isLoading"
         type="submit"
         :class="{
